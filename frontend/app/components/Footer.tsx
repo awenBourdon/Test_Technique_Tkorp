81e7bd2d440d8ed4026635a817c90f1e5957b6fe
awenafunagain <awen.bourdon@gmail.com>
2024-12-05 23:04:13 +0100

    some corrections

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -12,12 +12,12 @@ const Footer = () => {
             <p className="text-sm mt-1">Tous droits réservés.</p>
           </div>
           <div className="flex space-x-4">
-            <Link href="/privacy" className="relative group">
+            <Link href={"/privacy"} className="relative group">
               <span className="text-lg font-bold  tracking-wider bg-[#FD9745] border-2 rounded-md border-black px-3 py-1 transition-transform duration-200 inline-block hover:translate-x-1 hover:-translate-y-1 hover:shadow-[4px_4px_0_0_rgba(0,0,0,1)]">
                 Confidentialité
               </span>
             </Link>
-            <Link href="/terms" className="relative group">
+            <Link href={"/terms"} className="relative group">
               <span className="text-lg font-bold  tracking-wider bg-[#FD9745] border-2 rounded-md border-black px-3 py-1 transition-transform duration-200 inline-block hover:translate-x-1 hover:-translate-y-1 hover:shadow-[4px_4px_0_0_rgba(0,0,0,1)]">
                 Conditions
               </span>
--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 const Error404 = () => {
   return (
-    <div className="min-h-[90vh] flex flex-col items-center justify-center">
+    <div className="min-h-[75vh] flex flex-col items-center justify-center">
       <h1 className="text-6xl font-bold mb-12 tracking-tighter">Erreur 404. Page non trouvée.</h1>
       <p className="text-2xl font-semibold mb-12 tracking-tighter"> La page que vous recherchez n&apos;existe pas.</p>
-      <Link href="/" 
+      <Link href={"/"} 
         className="block mt-4 text-lg text-center bg-[#FD9745] border-2 rounded-md border-black p-2 font-bold  tracking-wider transition-transform duration-200 hover:translate-x-1 hover:-translate-y-1 hover:shadow-[4px_4px_0_0_rgba(0,0,0,1)]">
         Retour à l&apos;accueil
       </Link>
